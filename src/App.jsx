c77935943b876b50d6efc5581a38de74bad3ceff
Aster0N <nikvas131@gmail.com>
2025-03-19 23:38:56 +0300

    Added ability to on/off pointlight

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
 import Scene from "./components/Scene"
 
 function App() {
-  return <Scene></Scene>
+  const [isPointLight, setIsPointLight] = useState(false)
+
+  const switchPointLight = e => {
+    setIsPointLight(e.target.checked)
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="pointLightSwitch">
+          pointlight
+          <input
+            checked={isPointLight}
+            onChange={e => switchPointLight(e)}
+            type="checkbox"
+            id="pointLightSwitch"
+          />
+        </label>
+      </div>
+
+      <Scene isPointLight={isPointLight}></Scene>
+    </>
+  )
 }
 
 export default App
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -3,18 +3,20 @@ import Player from "@/components/Player/Player"
 import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 
-const Scene = () => {
+const Scene = ({ isPointLight }) => {
   return (
     <Canvas shadows style={{ height: "100vh" }}>
-      <ambientLight intensity={1} />
-      <pointLight
-        intensity={20}
-        position={[0, 5, 0]}
-        castShadow
-        shadow-mapSize={[1024, 1024]}
-        distance={20}
-        decay={1}
-      />
+      <ambientLight intensity={2} />
+      {isPointLight && (
+        <pointLight
+          intensity={20}
+          position={[0, 5, 0]}
+          castShadow
+          shadow-mapSize={[1024, 1024]}
+          distance={20}
+          decay={1}
+        />
+      )}
       <Player position={[0, 0, 0]} />
       <Ground />
       <OrbitControls />
